8788ed7880d660d0e16d06d833f3d5c1bbfd7d04
UmTaeho-Evaporation <nintendo7342@naver.com>
2020-11-05 17:41:41 +0900

    로그인 갱신

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import SignIn from './SignIn';
+import Login from './Login';
 import './App.css';
 class App extends React.Component {
   render() {
     return (
       <div>
-        <SignIn/>;
+        <Login/>;
       </div>
     );
   }
