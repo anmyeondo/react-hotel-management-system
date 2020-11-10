c4d429ba20e4ff321c989c7d87bce59994bf018d
UmTaeho-Evaporation <nintendo7342@naver.com>
2020-11-10 16:07:15 +0900

    components 이동 및 호텔 로고 적용

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Login from './Login';
+import Login from './components/Login';
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Reservation from './components/pages/Reservation';
-import Mypage from './components/pages/Mypage';
-import LogIn from './components/pages/LogIn';
-import SignUp from './components/pages/SignUp';
+import axios from "axios";
+
 
 class App extends React.Component {
   render() {
@@ -15,8 +13,12 @@ class App extends React.Component {
       <div>
         <Router>
           <Navbar />
-          <Login />;
-          </Router>
+          <Switch>
+            <Route path='/reservation' component={Reservation} />
+            <Login />;
+          </Switch>
+        </Router>
+
       </div>
     );
   }
--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
+  
   const showButton = () => {
     if (window.innerWidth <= 960) {
       setButton(false);
@@ -30,59 +30,13 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            <img src="https://logodix.com/logo/1931272.png" alt="" width='200' height='60' />
+            <img src="https://ifh.cc/g/tXuUTL.png" alt="" width='200' height='60' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link
-                to='/' className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/mypage'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                My Page
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/reservation'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Reservation
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/login'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                LOGIN
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/signup'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            </li>
           </ul>
-          {button && <Button2 buttonStyle='btn--outline'>LOGIN</Button2>}
-          {button && <Button1 buttonStyle='btn--outline'>SIGN UP</Button1>}
         </div>
       </nav>
     </>
