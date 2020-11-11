daabfc7d80ad7e91c6db8f06515b8f59eef7af81
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-11 23:51:54 +0900

    login api 연동

--- front-end/src/auth.js
+++ front-end/src/auth.js
@@ -1,13 +1,36 @@
-const users = [
-  { id: "qsc", password: "123", name: "Kim" },
-  { id: "wdv", password: "456", name: "Lee" },
-  { id: "efv", password: "789", name: "Park" },
-]
+import axios from "axios";
 
 export default function signIn({ id, password }) {
-  const user = users.find(
-    (user) => user.id === id && user.password === password
-  )
-  if (user === undefined) throw new Error()
-  return user
+  console.log("login start");
+  axios({
+    method: "get",
+    url: "/users/login",
+    params: {
+      id: id,
+      password: password,
+    },
+  })
+    .then((res) => {
+      // 응답 성공
+      console.log("응답 성공");
+
+      var data = res.data;
+      var ans = Object.keys(data).length;
+
+      if (ans == 0) {
+        console.log("no ID");
+        return 0;
+      } else if (data[0].phone == password) {
+        console.log("correct");
+        return 1;
+      } else {
+        console.log("pw error");
+        return 2;
+      }
+    })
+    .catch((error) => {
+      // 응답 실패(error)
+      console.log("error");
+    });
+  return false;
 }
