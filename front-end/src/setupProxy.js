282af92bad86b304c4dd7d80286324c6a7b9ceac
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-11 23:51:21 +0900

    proxy 추가 - /users

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -1,13 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    '/api/adminpage/',
-
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
-
+    createProxyMiddleware("/users", {
+      target: "http://localhost:5000/",
       changeOrigin: true,
-    }),
+    })
   );
 };
