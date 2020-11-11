e7c55979f4a163a6c9597399e01c4bc2ec2e2fe6
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-11 22:51:51 +0900

    login api 수정

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var connection = require('../database/database');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+// 로그인 API
+router.get('/login', (req, res, next) => {
+  console.log('login start...');
+  // const data = req.data; // {id: id, password: password}
+  const data = { id: 'KSH', password: '1234' };
+  const q = `select * from customer where name = "${data.id}"`;
+  var numID = 0;
+  connection.query(q, (err, rows, fields) => {
+    console.log(rows);
+    console.log(Object.keys(rows).length);
+    numID = numID + Object.keys(rows).length;
+  });
+  console.log(numID);
 });
 
 module.exports = router;
