c174ba2f1eed4eba7f7e4b0211cd101c4f39138e
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-15 17:53:11 +0900

    customers 라우터 추가

--- back-end/server.js
+++ back-end/server.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 // 라우터
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var customersRouter = require('./routes/customers');
 
 // Express 서버 실행
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/customers', customersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
