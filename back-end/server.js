fa111accf3122af733f56df53482a46530d4d510
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-15 18:46:17 +0900

    코드 정리

--- back-end/server.js
+++ back-end/server.js
@@ -9,13 +9,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var customersRouter = require('./routes/customers');
+var staffsRouter = require('./routes/staffs');
 
 // Express 서버 실행
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/customers', customersRouter);
+app.use('/staffs', staffsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -13,4 +13,10 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
+  app.use(
+    createProxyMiddleware("/staffs", {
+      target: "http://localhost:5000/",
+      changeOrigin: true,
+    })
+  );
 };
