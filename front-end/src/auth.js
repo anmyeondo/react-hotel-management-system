a3a662b539c69215e89d9931af295321cdf61946
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-17 17:22:59 +0900

    auth.js 로그인Api 적용

--- front-end/src/auth.js
+++ front-end/src/auth.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 
 export default async function signIn({ id, password }) {
-  console.log("login start");
+  console.log("로그인 API를 호출합니다");
   const res = await axios({
     method: "get",
-    url: "/users/login",
+    url: "/staffs/login",
     params: {
       id: id,
       password: password,
     },
   });
+
   // 응답 성공
-  console.log("응답 성공");
+  console.log("계정 조회 요청이 성공하였습니다");
+  const resData = JSON.parse(res.data);
+  const errorCode = resData.errorcode;
+  const compResult = resData.compResult;
 
-  var data = res.data;
-  var ans = data.length;
-
-  if (ans == 0) {
-    console.log("no ID");
-    return 0;
-  } else if (data[0].Staff_Password == password) {
-    console.log("correct");
-    return 1;
+  if (errorCode == 1) {
+    // 계정이 존재하지 않는 경우 처리
+  } else if (errorCode == 2) {
+    // 계정이 존재하는 경우 처리
   } else {
-    console.log("pw error");
-    return 2;
+    // 계정이 중복되는 경우 처리
   }
 }
