ebd6d1f2bcc015a859c4b64b02082ad43af1a7e0
UmTaeho-Evaporation <nintendo7342@naver.com>
2020-11-21 03:30:08 +0900

    20-11-21

--- back-end/routes/customers.js
+++ back-end/routes/customers.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var connection = require('../database/database');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/informs', (req, res, next) => {
+  var startTime = new Date();
+  console.log('Customers 테이블 조회를 시작합니다 : ' + startTime);
+  
+  const q = `SELECT * FROM Customer Natural Join Membership_Rank Natural Join Information Natural Join Membership_Validity Natural Join Zip`;
+
+  connection.query(q, (err, rows, fields) => {
+    res.send(JSON.stringify(rows));
+  });
+});
+
 
 module.exports = router;
