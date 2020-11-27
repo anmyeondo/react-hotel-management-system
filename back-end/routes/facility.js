bb0e54da2e6f725c462ec38c15c1c32f33f243a6
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-28 01:21:45 +0900

    레스토랑 추가 API 구현

--- back-end/routes/facility.js
+++ back-end/routes/facility.js
@@ -47,60 +47,31 @@ router.post('/restaurantSearch', multipartMiddleware, (req, res) => {
   console.log();
 });
 
-// /* 이미지 추가를 포함한 레스토랑 추가 */
-// router.post('/addRestaurant', upload.any(), async (req, res) => {
-//   const startTime = new Date();
-//   console.log('레스토랑 추가를 시작합니다 : ' + startTime);
+/* 이미지 추가를 포함한 레스토랑 추가 */
+router.post('/addRestaurant', upload.any(), async (req, res) => {
+  const startTime = new Date();
+  console.log('레스토랑 추가를 시작합니다 : ' + startTime);
 
-//   let image = '/image/' + 'basicImage';
-//   let body = req.body;
+  let image = '/facility_image/' + 'basicImage';
+  let body = req.body;
 
-//   if (Object(req.files).length > 0) {
-//     console.log('  이미지가 존재합니다. 경로를 설정합니다.');
-//     image = '/image/' + req.files[0].filename;
-//   } else {
-//     console.log('  이미지가 없습니다. 기본 이미지로 설정합니다.');
-//     delete body.staff_image;
-//   }
+  if (Object(req.files).length > 0) {
+    console.log('  이미지가 존재합니다. 경로를 설정합니다.');
+    image = '/facility_image/' + req.files[0].filename;
+  } else {
+    console.log('  이미지가 없습니다. 기본 이미지로 설정합니다.');
+    delete body.Restaurant_Img;
+  }
 
-//   // Staff 테이블에 데이터를 삽입하는 쿼리
-//   const q = `INSERT INTO Staff(Hotel_ID, Inform_ID, CODE, Rank, Bank, Account, Staff_Password, RegDate, Salary, Is_Available, Staff_Image) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, '${image}');`;
+  const value = Object.values(body);
 
-//   // 비밀번호 암호화 -> 암호화 비밀번호로 동기화 -> DB에 추가
-//   let pwEncrpt = async () => {
-//     console.log(' 비밀번호 암호화를 시작합니다');
-//     await bcrypt.genSalt(saltRounds, async (err, salt) => {
-//       await bcrypt.hash(body.staff_pw, salt, async (err, hash) => {
-//         console.log('  비밀번호가 암호화 되었습니다');
-//         let value = await makeValue(hash);
-//         let pushDB = await dbInsert(q, value);
-//         // return pushDB;
-//       });
-//     });
-//   };
+  // 레스토랑 추가 쿼리
+  // (Restaurant_Name, Hotel_ID, Open_Time, Close_Time, Available, Restaurant_Img)
+  const q = `INSERT INTO Restaurant VALUES(?, ?, ?, ?, ?, ?);`;
 
-//   // DB에 추가하는 메소드
-//   let dbInsert = async (q, value) => {
-//     console.log('  데이터베이스에 쿼리를 입력합니다');
-//     // console.log(value);
-//     connection.query(q, value, (err, rows, fields) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log('등록완료');
-//         res.send(rows);
-//       }
-//     });
-//   };
-
-//   // 암호화 비밀번호 동기화 메소드
-//   let makeValue = async (sp) => {
-//     console.log('  암호화된 비밀번호로 갱신중입니다');
-//     body.staff_pw = sp;
-//     return Object.values(body);
-//   };
-
-//   let ret = await pwEncrpt();
-// });
+  connection.query(q, value, (err, rows, fields) => {
+    res.json(rows);
+  });
+});
 
 module.exports = router;
--- back-end/server.js
+++ back-end/server.js
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/image', express.static('./image'));
+app.use('/facilityImage', express.static('./facility_image'));
 app.use(
   session({
     key: 'userid',
