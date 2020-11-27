40a7b00e99f7fb5622c34b6d799e9585f8271a7f
SeunghyunAnts <seunghyun4525@gmail.com>
2020-11-27 17:58:04 +0900

    예약 검색 API 제작

--- back-end/routes/reservations.js
+++ back-end/routes/reservations.js
@@ -30,4 +30,46 @@ router.get('/informs', (req, res, next) => {
   });
 });
 
+router.post('/searchReservation', multipartMiddleware, (req, res) => {
+  const startTime = new Date();
+  console.log('예약 정보 검색을 시작합니다 : ' + startTime);
+
+  const body = req.body; // { Check_In, Check_Out, Room_Type }
+  console.log(body);
+
+  let q = `SELECT * FROM Reservation NATURAL JOIN Room NATURAL JOIN Customer NATURAL JOIN Information NATURAL JOIN Zip NATURAL JOIN Card NATURAL JOIN Card_BIN`;
+  // let q = `SELECT * FROM Reservation NATURAL JOIN Room`;
+  let addq = ' WHERE';
+
+  for (let key in body) {
+    if (body[key] != '' && body[key] != undefined && body[key] != null) {
+      if (addq !== ' WHERE') {
+        addq = addq + ' and';
+      }
+      if (key === 'Check_In' || key === 'Check_Out') {
+        let values = JSON.parse(body[key]); // { Check, Start, End }
+        // console.log(!values.Check);
+        if (!values.Check) {
+          addq = addq + ` (${key} BETWEEN '${values.Start}' AND '${values.End}')`;
+        } else continue;
+      } else {
+        let x = body[key];
+        x = '"' + x + '"'; // 문자열 처리
+        addq = addq + ' ' + key + ' = ' + x;
+      }
+    } else {
+      continue;
+    }
+  }
+
+  if (addq != ' WHERE') {
+    q = q + addq;
+    console.log(q);
+    connection.query(q, (err, rows, fields) => {
+      console.log(JSON.stringify(rows));
+      res.send(JSON.stringify(rows));
+    });
+  }
+});
+
 module.exports = router;
